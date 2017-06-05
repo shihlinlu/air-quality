2909d8d950b0a788cb7fd036df046e8c0f8183cf
Shihlin Lu <shihlinlu@gmail.com>
2017-06-04 21:42:41 -0600

    added bootstrap, dependencies, and d3.js

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Latest compiled and minifed CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!-- [if lt IE9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery v3.0 -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
+
+		<link href="http://getbootstrap.com/examples/justified-nav/justified-nav.css" rel="stylesheet">
+
+		<!-- Latest compiled and minifed JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<style>
+			.axis path {
+				fill: none;
+				stroke: #777;
+				shape-rendering: crispEdges;
+			}
+			.axis text {
+				font-family: Arial;
+				font-size: 13px;
+			}
+		</style>
+
+	</head>
+
+	<body>
+		<div class="container">
+			<div class="jumbotron">
+				<svg id="visualization" width="1000" height="500"></svg>
+				<!-- Latest version of D3.js -->
+				<script src="//d3js.org/d3.v4.min.js"></script>
+
+			</div>
+		</div>
+	</body>
+</html>
